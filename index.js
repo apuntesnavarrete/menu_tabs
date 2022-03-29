4445bacf703c22145f9ad3693851a4e010b9cff4
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-03-28 18:25:59 -0600

    menu funcionando

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-alert("hola")
+let botones = document.getElementsByClassName("options")
+let contenedor = document.getElementsByClassName("contenidos")
+
+
+botones[0].addEventListener("click",()=>{
+    contenedor[0].style.display = "block"
+    contenedor[1].style.display = "none"    
+    contenedor[2].style.display = "none"
+    contenedor[3].style.display = "none"
+})
+
+botones[1].addEventListener("click",()=>{
+    contenedor[0].style.display = "none"
+    contenedor[1].style.display = "block"    
+    contenedor[2].style.display = "none"
+    contenedor[3].style.display = "none"
+})
+
+botones[2].addEventListener("click",()=>{
+    contenedor[0].style.display = "none"
+    contenedor[1].style.display = "none"    
+    contenedor[2].style.display = "block"
+    contenedor[3].style.display = "none"
+})
+
+botones[3].addEventListener("click",()=>{
+    contenedor[0].style.display = "none"
+    contenedor[1].style.display = "none"    
+    contenedor[2].style.display = "none"
+    contenedor[3].style.display = "block"
+})
+
+
