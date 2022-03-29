27a1c3a444676e105fbd9742c14b020eb5ff000b
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-03-29 01:38:12 -0600

    estructura basica

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ let botones = document.getElementsByClassName("options")
 let contenedor = document.getElementsByClassName("contenidos")
 
 
+
+    
 botones[0].addEventListener("click",()=>{
-    contenedor[0].style.display = "block"
-    contenedor[1].style.display = "none"    
-    contenedor[2].style.display = "none"
-    contenedor[3].style.display = "none"
+    let display = ["none","block"];
+    let ds = "block"
+    contenedor[0].style.display = display[1]
+    contenedor[1].style.display = display[0]    
+    contenedor[2].style.display = display[0]
+    contenedor[3].style.display = display[0]
 })
 
 botones[1].addEventListener("click",()=>{
@@ -29,5 +33,4 @@ botones[3].addEventListener("click",()=>{
     contenedor[2].style.display = "none"
     contenedor[3].style.display = "block"
 })
-
 
